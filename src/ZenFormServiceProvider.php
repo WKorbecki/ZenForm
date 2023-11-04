1005395babba275c3e141ca3d2c804b35a340f0f
WKorbecki <wojciechkorbecki@gmail.com>
2023-11-04 12:19:48 +0100

    fix service provider

--- src/ZenFormServiceProvider.php
+++ src/ZenFormServiceProvider.php
@@ -9,7 +9,11 @@ class ZenFormServiceProvider extends ServiceProvider {
         $this->publishes([
             __DIR__ . '/config/zen/form.php' => config_path('zen/form.php')
         ], 'config');
+        $this->publishes([
+            __DIR__ . '/resources/views' => resource_path('views/vendor/zenform')
+        ]);
 
+        $this->mergeConfigFrom(__DIR__ . '/config/zen/form.php', 'zen.form');
         $this->loadViewsFrom(__DIR__ . '/resources/views', 'zenform');
     }
 }
